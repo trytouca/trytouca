f42ba2de706a4dc6ed368271cb6ac2b4b0ee8e01
Pejman Ghorbanzade <pejman@touca.io>
2021-01-02 19:31:55 -0600

    examples: initial commit

---
+++ examples/basic/code_under_test.hpp
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2018-2020 Pejman Ghorbanzade. All rights reserved.
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+/**
+ *
+ */
+struct Date
+{
+    unsigned short _year;
+    unsigned short _month;
+    unsigned short _day;
+};
+
+/**
+ *
+ */
+struct Wizard
+{
+    std::string username;
+    std::wstring fullname;
+    Date dob;
+    float height;
+    double weight;
+    std::vector<std::string> wands;
+};
+
+/**
+ *
+ */
+Wizard parse_profile(const std::string& path);
+
+/**
+ *
+ */
+
+void custom_function_1(const Wizard& wizard);
+void custom_function_2(const Wizard& wizard);
+void custom_function_3(const Wizard& wizard);
---
+++ examples/basic/regression_test.hpp
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2018-2020 Pejman Ghorbanzade. All rights reserved.
+ */
+
+#pragma once
+
+#include "code_under_test.hpp"
+#include <weasel/weasel.hpp>
+
+template <>
+struct weasel::convert::Conversion<Date>
+{
+    std::shared_ptr<types::IType> operator()(const Date& value)
+    {
+        auto out = std::make_shared<types::Object>("Date");
+        out->add("year", value._year);
+        out->add("month", value._month);
+        out->add("day", value._day);
+        return out;
+    }
+};
