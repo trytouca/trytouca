f473430789a4f7eb7940ab346e1a3cd28f695fc7
Pejman Ghorbanzade <pejman@touca.io>
2021-01-08 21:29:53 -0600

    server: migrate chartjs to v3

--- app/src/app/home/pages/suite/chart.component.ts
+++ app/src/app/home/pages/suite/chart.component.ts
@@ -2,88 +2,102 @@
  * Copyright 2018-2020 Pejman Ghorbanzade. All rights reserved.
  */
 
-import { Component, Input } from '@angular/core';
-import { Chart } from 'chart.js';
+import { Component, Input, OnDestroy } from '@angular/core';
+import { CategoryScale, Chart,
+  LinearScale, LineController, LineElement, PointElement,
+  Tooltip, TooltipItem, Filler } from 'chart.js';
 import { DurationPipe } from 'src/app/home/pipes/duration.pipe';
+
+Chart.register(LineController, LinearScale, LineElement, CategoryScale,
+  PointElement, Tooltip, Filler);
 
 @Component({
   selector: 'app-suite-chart-runtime',
   templateUrl: './chart.component.html',
   providers: [ DurationPipe ]
 })
-export class SuiteChartRuntimeComponent {
+export class SuiteChartRuntimeComponent implements OnDestroy {
 
-  private lineChartTemplate: Chart.ChartConfiguration = {
-    type: 'line',
-    data: {
-      labels: [],
-      datasets: [{
-        label: 'Overall Execution Runtime',
-        data: [],
-        backgroundColor: [
-          'rgba(148,159,177,0.2)'
-        ],
-        borderColor: [
-          'rgba(148,159,177,1)'
-        ],
-        pointRadius: 4,
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        borderWidth: 1
-      }]
-    },
-    options: {
-      aspectRatio: 3,
-      responsive: true,
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'Overall Execution Runtime'
-          },
-          ticks: {
-            callback: (value: number, index, values) => {
-              return this.durationPipe.transform(value, 1, ['m', 's', 'ms']);
-            },
-            maxTicksLimit: 5
-          }
-        }]
-      },
-      tooltips: {
-        callbacks: {
-          label: (tooltipItem: Chart.ChartTooltipItem, data: Chart.ChartData) => {
-            const value = Number(tooltipItem.yLabel);
-            return this.durationPipe?.transform(value, 2, ['h', 'm', 's', 'ms']) || '';
-          }
-        },
-        displayColors: false
-      }
-    }
-  };
+  private chart: Chart;
 
   constructor(
     private durationPipe: DurationPipe,
   ) {
   }
 
+  ngOnDestroy() {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+  }
+
   @Input()
   set perfs(perfs: { slug: string, duration: number }[]) {
-    if (!perfs) {
+    if (!perfs || perfs.length == 0) {
       return;
     }
     perfs.reverse();
-    const durations = perfs.map(v => v.duration);
-    const labels = perfs.map(v => v.slug);
     const chartContext = (document.getElementById('suite-chart-runtime-trend') as HTMLCanvasElement).getContext('2d');
-    const chartConfig = this.lineChartTemplate;
-    chartConfig.data.datasets[0].data = durations;
-    chartConfig.data.labels = labels;
-    const runtimeChart = new Chart(chartContext, chartConfig);
+    this.chart = new Chart(chartContext, {
+      type: 'line',
+      data: {
+        labels: perfs.map(v => v.slug),
+        datasets: [{
+          backgroundColor: 'rgba(148,159,177,0.2)',
+          borderColor: 'rgba(148,159,177,1)',
+          borderWidth: 1,
+          tension: 0.25,
+          data: perfs.map(v => v.duration),
+          fill: 'origin',
+          label: 'Overall Execution Runtime',
+          pointRadius: 4,
+          pointBackgroundColor: 'rgba(148,159,177,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+        }]
+      },
+      options: {
+        aspectRatio: 3,
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            displayColors: false,
+            enabled: true,
+            intersect: false,
+            mode: 'nearest',
+            callbacks: {
+              label: (item: TooltipItem) => {
+                const point = item.dataPoint as { x: number, y: number };
+                return this.durationPipe?.transform(point.y, 2, ['h', 'm', 's', 'ms']) || '';
+              }
+            },
+          }
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: false
+        },
+        scales: {
+          y: {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Overall Execution Runtime'
+            },
+            ticks: {
+              callback: (value: number) => {
+                return this.durationPipe.transform(value, 1, ['m', 's', 'ms']);
+              },
+              maxTicksLimit: 5
+            }
+          }
+        }
+      }
+    });
   }
 
 }
