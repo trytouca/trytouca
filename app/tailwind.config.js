0d0b5ac0af69329d445e04099b8051ce7ef19461
Pejman Ghorbanzade <pejman@touca.io>
2021-01-11 22:05:49 -0600

    server: added tailwindcss to webapp

---
+++ app/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = (isProd) => ({
+    prefix: '',
+    purge: {
+      enabled: isProd,
+      content: [
+        './src/**/*.{html,ts}',
+      ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+});
