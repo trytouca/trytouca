6f49117780d6e2e849ad06ff9af6cfaddde9e85d
Pejman Ghorbanzade <pejman@touca.io>
2021-01-26 23:20:02 -0600

    server: use simple form of date-fns locale to improve impl of durationPipe

--- app/src/app/home/pipes/duration.pipe.ts
+++ app/src/app/home/pipes/duration.pipe.ts
@@ -10,42 +10,54 @@ import { intervalToDuration, formatDuration } from 'date-fns';
 })
 export class DurationPipe implements PipeTransform {
   /**
+   * simple `date-fns` locale that uses the abbreviated form of duration units
+   * such as `m` instead of `minutes`.
+   */
+  private formatDistance(token: string, count: number) {
+    const locale: Record<string, string> = {
+      xSeconds: 's',
+      xMinutes: 'm',
+      xHours: 'h'
+    };
+    const unit = token in locale ? locale[token] : '?';
+    return count.toString() + unit;
+  }
+
+  /**
    * Formats a given duration in milliseconds to a human-readable string.
    *
    * @param input duration in milliseconds
    * @param largest maximum number of units to use in description
    */
   transform(input: number, maxUnits = 2): string {
+    // return empty string if duration is 0.
     if (input === 0) {
       return '';
     }
+    // in this application, it is likely that most durations are extremely
+    // short. To save time, we check this scenario and cut short if duration
+    // is less than 1 second.
     if (input < 1000) {
       return `${input}ms`;
     }
-    const ms = input % 1000;
+    // in all other cases, we'd like to rely on `date-fns` to format the
+    // duration.
     const interval = intervalToDuration({ start: 0, end: input });
     const duration = formatDuration(interval, {
+      zero: false,
+      delimiter: ' ',
       format: ['hours', 'minutes', 'seconds'],
-      zero: false
+      locale: { formatDistance: this.formatDistance }
     });
-    const units: Record<string, string> = {
-      hour: 'h',
-      hours: 'h',
-      minute: 'm',
-      minutes: 'm',
-      second: 's',
-      seconds: 's',
-      ms: 'ms'
-    };
-    let parts = duration === '' ? [] : duration.split(' ');
+    // now we enforce our `maxUnits`
+    // at this point, since `date-fns` does not support millisecond precision,
+    // we manually append it before enforcing our `maxUnits` to adjust the
+    // level of detail in the final output.
+    const parts = duration.split(' ');
+    const ms = input % 1000;
     if (ms !== 0) {
-      parts.push(ms.toString(), 'ms');
+      parts.push(`${ms}ms`);
     }
-    parts = parts.slice(0, maxUnits * 2);
-    const output = [];
-    for (let i = 0; i < parts.length; i += 2) {
-      output.push(parts[i] + units[parts[i + 1]]);
-    }
-    return output.join(' ');
+    return parts.slice(0, maxUnits).join(' ');
   }
 }
