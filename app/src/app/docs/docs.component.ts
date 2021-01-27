ef9f2e2fc735df52ed1910f1866276fd2157a0a2
Pejman Ghorbanzade <pejman@touca.io>
2021-01-26 23:25:59 -0600

    server: fix url navigation for external links in docs component

--- app/src/app/docs/docs.component.ts
+++ app/src/app/docs/docs.component.ts
@@ -5,11 +5,16 @@
 import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faAngleDoubleLeft, faAngleDoubleRight, faBook, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+  faBook,
+  faExternalLinkAlt
+} from '@fortawesome/free-solid-svg-icons';
 import { Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-type InternalPage = { link: string, name: string, docs: string[] };
+type InternalPage = { link: string; name: string; docs: string[] };
 
 @Component({
   selector: 'app-page-docs',
@@ -17,11 +22,14 @@ type InternalPage = { link: string, name: string, docs: string[] };
   styleUrls: ['./docs.component.scss']
 })
 export class DocsComponent implements OnDestroy {
-
   readonly internalPages: InternalPage[] = [
     { link: 'intro', name: 'Introduction', docs: ['Introduction.md'] },
     { link: 'basics', name: 'Basic Terms', docs: ['BasicTerms.md'] },
-    { link: 'quickstart', name: 'Getting Started', docs: ['Quickstart_Cpp.md'] },
+    {
+      link: 'quickstart',
+      name: 'Getting Started',
+      docs: ['Quickstart_Cpp.md']
+    },
     { link: 'tips', name: 'Best Practices', docs: ['BestPractices.md'] },
     { link: 'features', name: 'New Features', docs: ['ReleaseNotes.md'] },
     { link: 'faq', name: 'FAQ', docs: ['Faq.md'] }
@@ -29,10 +37,10 @@ export class DocsComponent implements OnDestroy {
 
   readonly hiddenPages: InternalPage[] = [
     { link: 'terms', name: 'Terms of Service', docs: ['TermsOfService.md'] },
-    { link: 'privacy', name: 'Privacy Policy', docs: ['PrivacyPolicy.md'] },
+    { link: 'privacy', name: 'Privacy Policy', docs: ['PrivacyPolicy.md'] }
   ];
 
-  readonly externalPages: { name: string, path: string }[] = [
+  readonly externalPages: { name: string; path: string }[] = [
     { name: 'C++ Client API', path: '/docs/clients/cpp/index.html' },
     { name: 'Platform API', path: '/docs/backend/index.html' },
     { name: 'Product Roadmap', path: 'https://bit.ly/3q4EOcI' }
@@ -48,12 +56,20 @@ export class DocsComponent implements OnDestroy {
     private router: Router,
     private route: ActivatedRoute
   ) {
-    faIconLibrary.addIcons(faAngleDoubleLeft, faAngleDoubleRight, faBook, faExternalLinkAlt);
-    this._subQueryParamMap = this.route.queryParamMap.subscribe(map => {
-      const pages = [ ...this.internalPages, ...this.hiddenPages ];
-      const valid = pages.map(v => v.link);
-      const link = map.has('page') && valid.includes(map.get('page')) ? map.get('page') : 'quickstart';
-      const index = pages.findIndex(v => v.link === link);
+    faIconLibrary.addIcons(
+      faAngleDoubleLeft,
+      faAngleDoubleRight,
+      faBook,
+      faExternalLinkAlt
+    );
+    this._subQueryParamMap = this.route.queryParamMap.subscribe((map) => {
+      const pages = [...this.internalPages, ...this.hiddenPages];
+      const valid = pages.map((v) => v.link);
+      const link =
+        map.has('page') && valid.includes(map.get('page'))
+          ? map.get('page')
+          : 'quickstart';
+      const index = pages.findIndex((v) => v.link === link);
       this.currentPage = pages[index];
       this.previousPage = index === 0 ? null : pages[index - 1];
       this.nextPage = index === pages.length - 1 ? null : pages[index + 1];
@@ -73,11 +89,12 @@ export class DocsComponent implements OnDestroy {
   }
 
   launchExternalPage(name: string): void {
-    const path = this.externalPages.find(v => v.name === name).path;
-    const url = this.router.serializeUrl(this.router.createUrlTree([path]));
+    const path = this.externalPages.find((v) => v.name === name).path;
+    const url = path.startsWith('http')
+      ? path
+      : this.router.serializeUrl(this.router.createUrlTree([path]));
     if (environment.production) {
       window.open(url, '_blank');
     }
   }
-
 }
