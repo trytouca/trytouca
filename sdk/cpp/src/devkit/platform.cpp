8346ab8cd5e7f190b77c39eab2dc128f28017f2b
Pejman Ghorbanzade <pejman@touca.io>
2021-02-06 17:34:58 -0600

    sdk-cpp: update impl of request functions in http class

--- sdk/cpp/src/devkit/platform.cpp
+++ sdk/cpp/src/devkit/platform.cpp
@@ -32,16 +32,19 @@ namespace weasel {
     ResponseV2 HttpV2::get(const std::string& route) const
     {
         httplib::Client cli(_root.c_str());
-        if (!_token.empty()) {
-            cli.set_bearer_token_auth(_token.c_str());
-        }
-        httplib::Headers headers = {
+        cli.set_default_headers({
             { "Accept-Charset", "utf-8" },
             { "Accept", "application/json" },
             { "User-Agent", "weasel-client-cpp/1.2.1" }
-        };
-        const auto result = cli.Get(route.c_str(), headers);
-        return { result.value().status, result.value().body };
+        });
+        if (!_token.empty()) {
+            cli.set_bearer_token_auth(_token.c_str());
+        }
+        const auto result = cli.Get(route.c_str());
+        if (!result) {
+            return { -1, weasel::format("failed to submit HTTP GET request to {}", route) };
+        }
+        return { result->status, result->body };
     }
 
     /**
@@ -50,16 +53,19 @@ namespace weasel {
     ResponseV2 HttpV2::patch(const std::string& route, const std::string& body) const
     {
         httplib::Client cli(_root.c_str());
-        if (!_token.empty()) {
-            cli.set_bearer_token_auth(_token.c_str());
-        }
-        httplib::Headers headers = {
+        cli.set_default_headers({
             { "Accept-Charset", "utf-8" },
             { "Accept", "application/json" },
             { "User-Agent", "weasel-client-cpp/1.2.1" }
-        };
-        const auto result = cli.Patch(route.c_str(), headers, body, "application/json");
-        return { result.value().status, result.value().body };
+        });
+        if (!_token.empty()) {
+            cli.set_bearer_token_auth(_token.c_str());
+        }
+        const auto result = cli.Patch(route.c_str(), body, "application/json");
+        if (!result) {
+            return { -1, weasel::format("failed to submit HTTP PATCH request to {}", route) };
+        }
+        return { result->status, result->body };
     }
 
     /**
@@ -68,16 +74,19 @@ namespace weasel {
     ResponseV2 HttpV2::post(const std::string& route, const std::string& body) const
     {
         httplib::Client cli(_root.c_str());
-        if (!_token.empty()) {
-            cli.set_bearer_token_auth(_token.c_str());
-        }
-        httplib::Headers headers = {
+        cli.set_default_headers({
             { "Accept-Charset", "utf-8" },
             { "Accept", "application/json" },
             { "User-Agent", "weasel-client-cpp/1.2.1" }
-        };
-        const auto result = cli.Post(route.c_str(), headers, body, "application/json");
-        return { result.value().status, result.value().body };
+        });
+        if (!_token.empty()) {
+            cli.set_bearer_token_auth(_token.c_str());
+        }
+        const auto result = cli.Post(route.c_str(), body, "application/json");
+        if (!result) {
+            return { -1, weasel::format("failed to submit HTTP POST request to {}", route) };
+        }
+        return { result->status, result->body };
     }
 
     /**
@@ -86,16 +95,19 @@ namespace weasel {
     ResponseV2 HttpV2::binary(const std::string& route, const std::string& content) const
     {
         httplib::Client cli(_root.c_str());
-        if (!_token.empty()) {
-            cli.set_bearer_token_auth(_token.c_str());
-        }
-        httplib::Headers headers = {
+        cli.set_default_headers({
             { "Accept-Charset", "utf-8" },
             { "Accept", "application/json" },
             { "User-Agent", "weasel-client-cpp/1.2.1" }
-        };
-        const auto result = cli.Post(route.c_str(), headers, content, "application/octet-stream");
-        return { result.value().status, result.value().body };
+        });
+        if (!_token.empty()) {
+            cli.set_bearer_token_auth(_token.c_str());
+        }
+        const auto result = cli.Post(route.c_str(), content, "application/octet-stream");
+        if (!result) {
+            return { -1, weasel::format("failed to submit HTTP POST request to {}", route) };
+        }
+        return { result->status, result->body };
     }
 
     /**
@@ -193,34 +205,27 @@ namespace weasel {
     bool PlatformV2::handshake() const
     {
         const auto response = _http.get("/platform");
-
         if (response.status == -1) {
             _error = "the platform appears to be down";
             return false;
         }
-
         if (response.status != 200) {
             _error = "response from the platform is unexpected";
             return false;
         }
-
         rapidjson::Document doc;
         if (doc.Parse<0>(response.body.c_str()).HasParseError()) {
             _error = "failed to parse response from the platform";
             return false;
         }
-
         if (!doc.HasMember("ready") || !doc["ready"].IsBool()) {
             _error = "response form the platform is ill-formed";
             return false;
         }
-
         if (!doc["ready"].GetBool()) {
             _error = "platform is not ready";
             return false;
         }
-
-        // established connection with Weasel Platform
         return true;
     }
 
@@ -232,29 +237,23 @@ namespace weasel {
     {
         const auto content = weasel::format("{{\"key\": \"{}\"}}", apiKey);
         const auto response = _http.post("/client/signin", content);
-
-        // check status of response from Weasel Platform
-
+        if (response.status == -1) {
+            _error = "the platform appears to be down";
+            return false;
+        }
         if (response.status != 200) {
             _error = weasel::format("platform authentication failed: {}", response.status);
             return false;
         }
-
-        // Parse response from Weasel Platform
-
         rapidjson::Document doc;
         if (doc.Parse<0>(response.body.c_str()).HasParseError()) {
             _error = "failed to parse platform response";
             return false;
         }
-
-        // extract API Token issued by Weasel Platform
-
         if (!doc.HasMember("token") || !doc["token"].IsString()) {
             _error = "platform response malformed";
             return false;
         }
-
         _http.set_token(doc["token"].GetString());
         _is_auth = true;
         return true;
@@ -267,24 +266,15 @@ namespace weasel {
     {
         const auto& route = weasel::format("/element/{}/{}", _api._team, _api._suite);
         const auto response = _http.get(route);
-
-        // check status of response from Weasel Platform
-
         if (response.status != 200) {
             _error = "received unexpected platform response";
             return {};
         }
-
-        // parse response from Weasel Platform
-
         rapidjson::Document doc;
         if (doc.Parse<0>(response.body.c_str()).HasParseError()) {
             _error = "failed to parse response from the platform";
             return {};
         }
-
-        // extract list of element slugs in the response from Weasel Platform
-
         std::vector<std::string> elements;
         for (const auto& rjElement : doc.GetArray()) {
             elements.emplace_back(rjElement["name"].GetString());
