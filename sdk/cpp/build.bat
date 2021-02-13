a37b953057a8af3a45dae4e6fed5e300f5639ebe
Pejman Ghorbanzade <pejman@touca.io>
2021-02-13 13:10:31 -0600

    sdk-cpp: batch script should exit upon failure

--- sdk/cpp/build.bat
+++ sdk/cpp/build.bat
@@ -1,30 +1,43 @@
+@REM
+@REM Copyright 2018-2020 Pejman Ghorbanzade. All rights reserved.
+@REM
+
+@ECHO OFF
 SETLOCAL ENABLEEXTENSIONS
-SET me=%~n0
-SET parent=%~dp0
+SET dir_script=%~dp0
+SET dir_bin="%dir_script%local\bin"
+SET dir_build="%dir_script%local\build"
+SET dir_dist="%dir_script%local\dist"
+SET dir_export="%dir_build%conan-export-pkg"
 
-if exist .\local\bin rmdir .\local\bin /q /s
-if exist .\local\build rmdir .\local\build /q /s
-if exist .\local\dist rmdir .\local\dist /q /s
-if not exist .\local\build\logs mkdir .\local\build\logs
+FOR %%D IN (%dir_bin%, %dir_build%, %dir_dist%, %dir_export%) DO (
+    if exist %%D rmdir %%D /q /s
+    mkdir %%D
+)
 
-conan install ^
-    -o shared=True ^
+conan install -o shared=True ^
     -o with_tests=True ^
     -o with_utils=True ^
     -o with_examples=True ^
     -o with_framework=True ^
-    --install-folder .\local\build .\conanfile.py --build=missing
+    --install-folder "%dir_build%" ^
+    "%dir_script%conanfile.py" --build=missing ^
+    || (echo "failed to install dependencies using conan" && exit /b !ERRORLEVEL!)
 
-cmake -B".\local\build" -H"." -G"Visual Studio 15 2017 Win64" ^
+cmake -B".\local\build" -H"." -G"Visual Studio 16 2019" -A"x64" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_SHARED_LIBS=ON ^
     -DWEASEL_BUILD_TESTS=ON ^
     -DWEASEL_BUILD_UTILS=ON ^
     -DWEASEL_BUILD_EXAMPLES=ON ^
-    -DWEASEL_BUILD_FRAMEWORK=ON
+    -DWEASEL_BUILD_FRAMEWORK=ON ^
+    || (echo "failed to configure cmake" && exit /b !ERRORLEVEL!)
 
-cmake --build ".\local\build" --config Release --parallel ^
-     > .\local\build\logs\weasel.build.stdout ^
-     2> .\local\build\logs\weasel.build.stderr
+cmake --build "%dir_build%" --config Release --parallel ^
+    || (echo "failed to build the library" && exit /b !ERRORLEVEL!)
 
-cmake --install ".\local\build" --prefix ".\local\dist"
+cmake --install "%dir_build%" --prefix "%dir_dist%" ^
+    || (echo "failed to install build artifacts" && exit /b !ERRORLEVEL!)
+
+conan export-pkg -if %dir_build% -bf "%dir_export%" -f "%dir_script%conanfile.py" ^
+    || (echo "failed to create conan package" && exit /b !ERRORLEVEL!)
