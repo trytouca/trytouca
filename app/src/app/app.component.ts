8cf7745d9aa38c6c95c298703b5ef0e4126582ad
Pejman Ghorbanzade <pejman@touca.io>
2021-02-20 00:52:57 -0600

    server: add google tag manager

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -2,30 +2,26 @@
  * Copyright 2018-2020 Pejman Ghorbanzade. All rights reserved.
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { filter, map, mergeMap } from 'rxjs/operators';
-import { UserService } from './core/services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   appNameLogo = 'Weasel';
   appNameBrief = 'Weasel';
   appNameVerbose = 'Weasel';
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private router: Router,
     private titleService: Title,
-    private userService: UserService
-  ) {}
-
-  ngOnInit() {
-    this.router.events
+    router: Router
+  ) {
+    router.events
       .pipe(
         filter((event) => event instanceof NavigationEnd),
         map(() => this.activatedRoute),
