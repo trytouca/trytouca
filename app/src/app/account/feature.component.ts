371a89fb45ee75eda6ec9a20b78bbc20d89c8696
Pejman Ghorbanzade <pejman@touca.io>
2021-02-27 15:49:00 -0600

    server: change badge color in feature component

--- app/src/app/account/feature.component.ts
+++ app/src/app/account/feature.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
   selector: 'wsl-account-feature',
   templateUrl: './feature.component.html'
 })
-export class FeatureComponent {}
+export class FeatureComponent {
+  features: string[] = [
+    'Our platform now creates a team and populates it with sample test results for new users.',
+    'You can now remove test results submitted for any given version right from the user interface.',
+    'View your API Key and API URL and copy them to clipboard at any time from the suite page.',
+    'Building our Client Library for C++ now only requires CMake.',
+    'Our Test Framework for C++ can now seal the version after executing all test cases.'
+  ];
+}
