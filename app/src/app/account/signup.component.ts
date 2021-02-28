8a99020f8f26771c09fcebe245cc0c321ba3dde1
Pejman Ghorbanzade <pejman@touca.io>
2021-02-27 21:02:44 -0600

    server: show mail svg when signup form is submitted

--- app/src/app/account/signup.component.ts
+++ app/src/app/account/signup.component.ts
@@ -2,8 +2,9 @@
  * Copyright 2018-2020 Pejman Ghorbanzade. All rights reserved.
  */
 
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { timer, Subscription } from 'rxjs';
 import { ApiService } from '@weasel/core/services';
 import { Alert } from '@weasel/shared/components/alert.component';
 
@@ -16,7 +17,7 @@ interface FormContent {
   templateUrl: './signup.component.html',
   styles: []
 })
-export class SignupComponent {
+export class SignupComponent implements OnDestroy {
   formSignup = new FormGroup({
     umail: new FormControl('', {
       validators: [Validators.required, Validators.email],
@@ -24,13 +25,23 @@ export class SignupComponent {
     })
   });
   alert: Alert;
-  submitted: boolean;
-  prev: FormContent;
+  isFormShown = true;
+  isBackButtonShown = false;
+  subBackButton: Subscription;
 
   /**
    *
    */
   constructor(private apiService: ApiService) {}
+
+  /**
+   *
+   */
+  ngOnDestroy() {
+    if (this.subBackButton) {
+      this.subBackButton.unsubscribe();
+    }
+  }
 
   /**
    *
@@ -42,19 +53,39 @@ export class SignupComponent {
     if (!this.formSignup.valid) {
       return;
     }
-    if (this.prev === model) {
-      return;
-    }
-    this.submitted = true;
-    this.prev = model;
-    console.log(model);
+    this.isFormShown = false;
+    this.subBackButton = timer(30000).subscribe(() => {
+      this.isBackButtonShown = true;
+    });
   }
 
   /**
-   *
+   * Determines if help tip should be shown below the input field.
    */
-  isStartFormValid() {
+  isFormValid() {
     const field = this.formSignup.controls['umail'];
     return field.pristine || field.valid;
+  }
+
+  /**
+   * Allows user to go back to the signup form once they have registered and
+   * submitted the form.
+   */
+  showForm() {
+    this.isBackButtonShown = false;
+    this.isFormShown = true;
+    this.formSignup.reset();
+  }
+
+  /**
+   * Allows user to request another welcome email in case the original email
+   * did not get through.
+   *
+   * @todo instead of attempting to re-register user, we should create a
+   *       separate backend route that only resends the welcome email.
+   */
+  resendEmail() {
+    this.isBackButtonShown = false;
+    this.onSubmit(this.formSignup.value);
   }
 }
