c5cc614fddd9fe4bbd4e06f1da34f9b4ab0fb0de
Pejman Ghorbanzade <pejman@touca.io>
2021-03-03 00:16:33 -0600

    server: update navigation route to signin page

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -38,7 +38,9 @@ export class HomeComponent {
           this.authService.logout().subscribe(() => {
             this.alertService.reset();
             this.userService.reset();
-            this.router.navigate(['signin'], { queryParams: { e: '401' } });
+            this.router.navigate(['account', 'signin'], {
+              queryParams: { e: '401' }
+            });
           });
         });
       }
