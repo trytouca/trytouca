3f648da7f094284290adbcc4af2f2bfb6c020220
Pejman Ghorbanzade <pejman@touca.io>
2021-04-08 02:42:28 -0500

    web: enable nextjs strict mode

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   future: {
-    webpack5: true,
+    webpack5: true
   },
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/docs',
+        destination: 'https://docs.getweasel.com/',
+        permanent: true
+      }
+    ];
+  }
 };
