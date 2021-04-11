1e0d2c0581fcf52f3ec3ae0cc3e111003b2eea39
Pejman Ghorbanzade <pejman@touca.io>
2021-04-11 00:17:05 -0500

    web: port testimonial to landing page

--- web/components/footer-cta.tsx
+++ web/components/footer-cta.tsx
@@ -4,12 +4,12 @@
 
 export default function FooterCta() {
   return (
-    <div className="space-y-2 text-white">
+    <div className="text-white space-y-4">
       <h3 className="wsl-text-shadow text-2xl xl:text-3xl font-bold">
         Try Weasel today
       </h3>
-      <p className="text-lg">
-        We help teams maintain software more efficiently.
+      <p className="text-xl">
+        We make maintaining software 10x more efficient.
       </p>
       <button className="wsl-btn-green px-4 py-2 text-base leading-6 rounded-lg shadow-md focus:ring-2 focus:ring-opacity-50 focus:ring-light-blue-400">
         Get Started
