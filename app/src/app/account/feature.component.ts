c28740e73100fe31bca84694cf18e163786df66c
Pejman Ghorbanzade <pejman@touca.io>
2021-04-30 19:24:19 -0700

    server: Update new features in sign-in page

--- app/src/app/account/feature.component.ts
+++ app/src/app/account/feature.component.ts
@@ -10,10 +10,10 @@ import { Component } from '@angular/core';
 })
 export class FeatureComponent {
   features: string[] = [
+    'Self-hosted deployments now require less than 1 GB of memory instead of 4 GB.',
     'Our platform now creates a team and populates it with sample test results for new users.',
     'You can now remove test results submitted for any given version right from the user interface.',
     'View your API Key and API URL and copy them to clipboard at any time from the suite page.',
-    'Building our Client Library for C++ now only requires CMake.',
-    'Our Test Framework for C++ can now seal the version after executing all test cases.'
+    'You can now change your fullname or username from the Account Settings page.'
   ];
 }
