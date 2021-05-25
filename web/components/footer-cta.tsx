4a46785bbeafd4b35c27edb418063a19e91bb9e0
Pejman Ghorbanzade <pejman@touca.io>
2021-05-24 22:18:32 -0700

    web: Add product hunt badge

--- web/components/footer-cta.tsx
+++ web/components/footer-cta.tsx
@@ -2,26 +2,39 @@
  * Copyright 2018-2020 Pejman Ghorbanzade. All rights reserved.
  */
 
+import { ProductHunt } from '@/lib/product-hunt';
+
 export default function FooterCta() {
   return (
-    <div className="text-white space-y-4">
-      <h3 className="wsl-text-shadow text-2xl xl:text-3xl font-bold">
-        Try Touca Today
-      </h3>
-      <p className="text-xl">
-        We make maintaining software 10x more efficient.
-      </p>
-      <div>
-        <a
-          href="https://app.touca.io"
-          target="_blank"
-          rel="noopener noreferrer">
-          <button
-            className="wsl-btn-green px-4 py-2 text-base leading-6 rounded-lg shadow-md focus:ring-2 focus:ring-opacity-50 focus:ring-light-blue-400"
-            type="button">
-            Get Started
-          </button>
-        </a>
+    <div className="py-8 flex justify-between">
+      <div className="text-white space-y-4">
+        <h3 className="wsl-text-shadow text-2xl xl:text-3xl font-bold">
+          Try Touca Today
+        </h3>
+        <p className="text-xl">
+          We make maintaining software 10x more efficient.
+        </p>
+        <div>
+          <a
+            href="https://app.touca.io"
+            target="_blank"
+            rel="noopener noreferrer">
+            <button
+              className="wsl-btn-green px-4 py-2 text-base leading-6 rounded-lg shadow-md focus:ring-2 focus:ring-opacity-50 focus:ring-light-blue-400"
+              type="button">
+              Get Started
+            </button>
+          </a>
+        </div>
+      </div>
+      <div className="px-16">
+        <h4 className="text-2xl text-white font-bold">
+          We are live on Product Hunt today!
+        </h4>
+        <p className="pb-4 text-xl text-light-blue-200">
+          Send us good vibes if you feel like it.
+        </p>
+        <ProductHunt />
       </div>
     </div>
   );
---
+++ web/lib/product-hunt.tsx
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2018-2020 Pejman Ghorbanzade. All rights reserved.
+ */
+
+export const ProductHunt = () => {
+  return (
+    <div>
+      <a
+        href="https://www.producthunt.com/posts/touca?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-touca"
+        target="_blank"
+        rel="noopener noreferrer">
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=296653&theme=dark"
+          alt="Touca - Automated Regression Testing for Critical Software Workflows | Product Hunt"
+          width="250"
+          height="54"
+        />
+      </a>
+    </div>
+  );
+};
