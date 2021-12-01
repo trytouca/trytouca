15f706d819303f48fef634abd17ea27f7102f1c6
Pejman Ghorbanzade <pejman@touca.io>
2021-11-30 18:47:29 -0600

    web: Ensure Google Tag Manager is loaded before submitting events

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -24,18 +24,16 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {tracker.enabled() && (
-        <>
-          <Script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer',${GTM_ID});`
-            }}
-            strategy="afterInteractive"
-          />
-        </>
+          }}
+          strategy="afterInteractive"
+        />
       )}
       <Component {...pageProps} />
     </>
