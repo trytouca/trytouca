0c52d98e7fffd104d3bb0d8d6bf44a34ec338259
Pejman Ghorbanzade <pejman@touca.io>
2021-12-15 15:11:44 -0600

    sdk-python: Update API endpoint to obtain list of test cases

--- sdk/python/touca/_transport.py
+++ sdk/python/touca/_transport.py
@@ -5,6 +5,7 @@ import json
 from .__init__ import __version__ as client_version
 from typing import List
 from urllib3.poolmanager import PoolManager
+from urllib3.response import HTTPResponse
 
 
 class Transport:
@@ -18,7 +19,6 @@ class Transport:
         self._handshake()
 
     def _handshake(self):
-
         response = self._send_request("GET", "/platform")
         if response.status != 200:
             raise RuntimeError("could not communicate with touca server")
@@ -26,8 +26,9 @@ class Transport:
         if not content["ready"]:
             raise RuntimeError("touca server is not ready")
 
-    def _send_request(self, method, path, body=None, content_type="application/json"):
-
+    def _send_request(
+        self, method, path, body=None, content_type="application/json"
+    ) -> HTTPResponse:
         if body and content_type == "application/json":
             body = json.dumps(body).encode("utf-8")
         headers = {
@@ -46,7 +47,6 @@ class Transport:
         )
 
     def update_options(self, options: dict):
-
         if any(k in options for k in ["api_key", "api_url"]):
             self._token = None
             self._handshake()
@@ -71,28 +71,32 @@ class Transport:
         self._token = body["token"]
 
     def get_testcases(self) -> List[str]:
-
         team = self._options.get("team")
         suite = self._options.get("suite")
-        response = self._send_request(method="GET", path=f"/element/{team}/{suite}")
+        response = self._send_request(
+            method="GET", path=f"/client/element/{team}/{suite}"
+        )
         if response.status != 200:
             raise RuntimeError("Failed to obtain list of test cases")
         body = json.loads(response.data.decode("utf-8"))
         return [k["name"] for k in body]
 
     def post(self, content):
-
         response = self._send_request(
             method="POST",
             path=f"/client/submit",
             body=content,
             content_type="application/octet-stream",
         )
-        if response.status != 204:
-            raise RuntimeError("failed to submit test results")
+        if response.status == 204:
+            return
+        if response.status == 400 and "batch is sealed" in response.data.decode(
+            "utf-8"
+        ):
+            reason = " This version is already submitted and sealed."
+        raise RuntimeError(f"Failed to submit test results.{reason}")
 
     def seal(self):
-
         slugs = "/".join(self._options.get(k) for k in ["team", "suite", "version"])
         response = self._send_request(method="POST", path=f"/batch/{slugs}/seal2")
         if response.status != 204:
