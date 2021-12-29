31ef92671bb2dc0abd897f33dcdc897f15f933aa
Pejman Ghorbanzade <pejman@touca.io>
2021-12-29 10:28:09 -0600

    server: Add logic to generate PDF report for a given version

--- api/src/controllers/batch/export_pdf.ts
+++ api/src/controllers/batch/export_pdf.ts
@@ -6,7 +6,9 @@ import mustache from 'mustache'
 import path from 'path'
 import Puppeteer from 'puppeteer'
 
-import { IBatchDocument } from '@/schemas/batch'
+import { ComparisonFunctions } from '@/controllers/comparison'
+import { UserMap } from '@/models/usermap'
+import { BatchModel, IBatchDocument } from '@/schemas/batch'
 import { ISuiteDocument } from '@/schemas/suite'
 import { ITeam } from '@/schemas/team'
 import { IUser } from '@/schemas/user'
@@ -27,12 +29,19 @@ interface PageData {
       name: string
     }
     fresh: {
-      name: string
-    }[]
+      hasCases: boolean
+      cases: {
+        name: string
+      }[]
+    }
     missing: {
-      name: string
-    }[]
+      hasCases: boolean
+      cases: {
+        name: string
+      }[]
+    }
     behavior: {
+      hasCases: boolean
       cases: {
         name: string
         matchRate: string
@@ -41,6 +50,7 @@ interface PageData {
     duration: {
       value: string
       change: string
+      hasCases: boolean
       cases: {
         name: string
         value: string
@@ -50,54 +60,78 @@ interface PageData {
   }
 }
 
-async function buildPageData(suite: ISuiteDocument): Promise<PageData> {
+async function buildPageData(
+  suite: ISuiteDocument,
+  srcBatch: IBatchDocument
+): Promise<PageData> {
+  const userMap = await new UserMap()
+    .addGroup('submittedBy', srcBatch.submittedBy)
+    .populate()
+  const submittedAt = srcBatch.submittedAt.toLocaleString('en-US', {
+    dateStyle: 'full',
+    timeZone: 'UTC',
+    timeStyle: 'long'
+  })
+  const submittedBy = {
+    name: userMap
+      .getGroup('submittedBy')
+      .map((v) => v.fullname)
+      .join(' and ')
+  }
+  const dstBatch = await BatchModel.findOne(
+    { _id: suite.promotions[suite.promotions.length - 1].to },
+    { _id: 1, slug: 1 }
+  )
+  const cmpOutput = await ComparisonFunctions.compareBatch(
+    dstBatch._id,
+    srcBatch._id
+  )
+  const differentCases = cmpOutput.common.filter((v) => v.meta.keysScore !== 1)
+  const behavior = {
+    hasCases: differentCases.length !== 0,
+    cases: differentCases.map((v) => ({
+      name: v.src.elementName,
+      matchRate: Math.floor(v.meta.keysScore * 100).toString() + '%'
+    }))
+  }
+  const duration = {
+    value: cmpOutput.overview.metricsDurationHead.toString(),
+    change: cmpOutput.overview.metricsDurationChange.toString(),
+    hasCases: cmpOutput.common.length !== 0,
+    cases: cmpOutput.common.map((v) => ({
+      name: v.src.elementName,
+      value: v.meta.metricsDurationCommonSrc.toString(),
+      change:
+        (
+          ((v.meta.metricsDurationCommonSrc - v.meta.metricsDurationCommonDst) /
+            v.meta.metricsDurationCommonDst) *
+          100
+        ).toFixed(2) + '%'
+    }))
+  }
+  const fresh = {
+    hasCases: cmpOutput.fresh.length !== 0,
+    cases: cmpOutput.fresh.map((v) => ({ name: v.elementName }))
+  }
+  const missing = {
+    hasCases: cmpOutput.missing.length !== 0,
+    cases: cmpOutput.missing.map((v) => ({ name: v.elementName }))
+  }
   const content: PageData = {
     suite: {
       name: suite.name
     },
     dstBatch: {
-      name: 'vDstBatch'
+      name: dstBatch.slug
     },
     srcBatch: {
-      name: 'vSrcBatch',
-      submittedAt: 'December 23, 2021 at 10:55 PM',
-      submittedBy: {
-        name: 'Pejman Ghorbanzade'
-      },
-      fresh: [
-        {
-          name: 'charli'
-        }
-      ],
-      missing: [
-        {
-          name: 'charlie'
-        }
-      ],
-      behavior: {
-        cases: [
-          {
-            name: 'alice',
-            matchRate: '44.4%'
-          }
-        ]
-      },
-      duration: {
-        value: '1s 832ms',
-        change: 'same as',
-        cases: [
-          {
-            name: 'alice',
-            value: '1s 359ms',
-            change: ''
-          },
-          {
-            name: 'bob',
-            value: '1s 405ms',
-            change: '4% slower'
-          }
-        ]
-      }
+      name: srcBatch.slug,
+      submittedAt,
+      submittedBy,
+      behavior,
+      duration,
+      fresh,
+      missing
     }
   }
   return content
@@ -112,21 +146,20 @@ export async function ctrlBatchExportPDF(
   const team = res.locals.team as ITeam
   const suite = res.locals.suite as ISuiteDocument
   const batch = res.locals.batch as IBatchDocument
-  const tuple = [team.slug, suite.slug, batch.slug].join('/')
   const filename = ['touca', suite.slug, batch.slug].join('_') + '.pdf'
-  logger.debug('%s: %s: exporting as pdf', user.username, tuple)
+  logger.debug('%s: exporting %s', user.username, filename)
 
   if (!batch.sealedAt) {
     return next({ errors: ['batch is not sealed'], status: 400 })
   }
 
-  const sample_html_file = path.join(
+  const content = await buildPageData(suite, batch)
+  const template_file = path.join(
     config.mail.templatesDirectory,
     'reports',
-    'sample.html'
+    'report.html'
   )
-  const content = await buildPageData(suite)
-  const template = readFileSync(sample_html_file, 'utf-8')
+  const template = readFileSync(template_file, 'utf-8')
   const bodyHtml = mustache.render(template, { content })
 
   const browser = await Puppeteer.launch({ headless: true })
@@ -139,5 +172,5 @@ export async function ctrlBatchExportPDF(
   res.setHeader('Content-Type', 'application/pdf')
   res.writeHead(200)
   res.end(buffer)
-  logger.info('%s: %s: exported as pdf', user.username, tuple)
+  logger.info('%s: %s: exported %s', user.username, filename)
 }
