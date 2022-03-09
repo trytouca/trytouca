edc78548b2e1edbe5d841eb0f96e87902cc4cc26
Pejman Ghorbanzade <pejman@touca.io>
2022-03-09 02:49:17 -0600

    web: Remove Mixpanel from marketing site

--- web/lib/tracker.tsx
+++ web/lib/tracker.tsx
@@ -1,29 +1,15 @@
 // Copyright 2021 Touca, Inc. Subject to Apache-2.0 License.
 
-import mixpanel from 'mixpanel-browser';
-
-const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
-
 declare global {
   interface Window {
     plausible: (action: string, props: Record<string, unknown>) => void;
   }
 }
 
-class Tracker {
-  constructor() {
-    if (MIXPANEL_TOKEN) {
-      mixpanel.init(MIXPANEL_TOKEN);
-    }
-  }
+export const tracker = {
   track(event: { action: string }, data?: Record<string, unknown>): void {
-    if (MIXPANEL_TOKEN) {
-      mixpanel.track(event.action, data);
-    }
-    if (window.plausible) {
+    if (typeof window !== 'undefined' && window.plausible) {
       window.plausible(event.action, { props: data });
     }
   }
-}
-
-export const tracker = new Tracker();
+};
