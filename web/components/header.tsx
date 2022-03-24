301a085594d7926ba6837c0836ff428945e46897
Pejman Ghorbanzade <pejman@touca.io>
2022-03-23 22:57:40 -0500

    change blog link in header back to ghost

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -9,7 +9,7 @@ import { HiMenu, HiOutlineNewspaper } from 'react-icons/hi';
 const items = [
   { title: 'Login', link: 'https://app.touca.io' },
   { title: 'Docs', link: 'https://touca.io/docs' },
-  { title: 'Blog', link: '/blog' },
+  { title: 'Blog', link: 'https://blog.touca.io' },
   { title: 'Pricing', link: '/pricing' }
 ];
 
