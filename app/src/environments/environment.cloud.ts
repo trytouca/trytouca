95e9d218eb795dc5070f2c6b87c320435f037881
Pejman Ghorbanzade <pejman@touca.io>
2022-03-28 10:19:14 -0500

    server: Add cloud build configuration for frontend

---
+++ app/src/environments/environment.cloud.ts
@@ -0,0 +1,10 @@
+// Copyright 2022 Touca, Inc. Subject to Apache-2.0 License.
+
+export const environment = {
+  apiUrl: 'https://api.touca.io',
+  appVersion: '1.4.0',
+  dataRefreshInterval: 10000,
+  google_api_client_id:
+    '232474004917-li0ioanjkoi6lj6vq2intei2kv7op5li.apps.googleusercontent.com',
+  production: true
+};
