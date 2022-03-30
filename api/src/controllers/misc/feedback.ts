e68f3b24715fd97bd38362d2fd3146c56573e56a
Pejman Ghorbanzade <pejman@touca.io>
2022-03-29 20:44:10 -0500

    server: Relay feedback to platform owner

--- api/src/controllers/misc/feedback.ts
+++ api/src/controllers/misc/feedback.ts
@@ -3,7 +3,8 @@
 import { NextFunction, Request, Response } from 'express'
 
 import { relay } from '@/models/relay'
-import { wslFindByUname } from '@/models/user'
+import { wslFindByRole } from '@/models/user'
+import { EPlatformRole } from '@/types/commontypes'
 import { config } from '@/utils/config'
 import logger from '@/utils/logger'
 import * as mailer from '@/utils/mailer'
@@ -24,8 +25,8 @@ export async function feedback(
 
   // we are intentionally not awaiting on this operation
   if (config.isCloudHosted) {
-    const superuser = await wslFindByUname('touca')
-    mailer.mailUser(superuser, 'New User Feedback', 'user-feedback', {
+    const owners = await wslFindByRole(EPlatformRole.Owner)
+    mailer.mailUser(owners[0], 'New User Feedback', 'user-feedback', {
       body: content.body,
       name: content.name,
       page: content.page,
