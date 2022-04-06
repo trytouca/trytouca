3cd45508468472a75bbeab0082ef684377552c69
Pejman Ghorbanzade <pejman@touca.io>
2022-04-06 00:34:32 -0500

    server: Update cache policy for web app

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -4,7 +4,6 @@ export const environment = {
   apiUrl: 'http://localhost:8081',
   appVersion: '1.4.0',
   dataRefreshInterval: 10000,
-  google_api_client_id:
-    '232474004917-li0ioanjkoi6lj6vq2intei2kv7op5li.apps.googleusercontent.com',
+  google_api_client_id: '',
   production: false
 };
