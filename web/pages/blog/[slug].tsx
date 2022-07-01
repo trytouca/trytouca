31442bfb5862f04cdb9a9f30dec8f838dece3acf
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2022-07-01 01:22:23 -0700

    add prism syntax highlighting

--- web/pages/blog/[slug].tsx
+++ web/pages/blog/[slug].tsx
@@ -3,6 +3,7 @@
 import { PostOrPage } from '@tryghost/content-api';
 import { GetStaticPropsContext } from 'next';
 import Head from 'next/head';
+import Script from 'next/script';
 import { ArticleJsonLd, BreadcrumbJsonLd, NextSeo } from 'next-seo';
 import { HiOutlineCalendar, HiOutlineClock } from 'react-icons/hi';
 
@@ -125,6 +126,18 @@ export default function BlogPage(props: BlogPostStaticProps) {
 function BlogPostContent(props: { article: PostOrPage }) {
   return (
     <>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/themes/prism-okaidia.min.css"
+      />
+      <Script
+        strategy="afterInteractive"
+        src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/components/prism-core.min.js"
+      />
+      <Script
+        strategy="afterInteractive"
+        src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/plugins/autoloader/prism-autoloader.min.js"
+      />
       <article
         className="prose mx-auto lg:prose-lg"
         dangerouslySetInnerHTML={{ __html: props.article.html }}
