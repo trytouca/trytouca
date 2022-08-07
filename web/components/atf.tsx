2b29783a6ec5a22ae92153625df8ed43b003d814
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2022-08-07 14:10:13 -0700

    web: update signup cta

--- web/components/atf.tsx
+++ web/components/atf.tsx
@@ -8,8 +8,8 @@ import { ATFBackedBy, ATFTrustedBy } from './social-proof';
 function ATFButtonDemo() {
   return (
     <Link href="/demo">
-      <a className="text-md flex items-center shadow-[0_0_2px_#7dd3fc] hover:shadow-[0_0_3px_#7dd3fc] box-shadow group space-x-2 rounded-xl bg-opacity-50 p-3 font-medium text-gray-300 duration-150 ease-in-out hover:text-white focus:outline-none">
-        <span>Schedule a Demo</span>
+      <a className="text-md flex items-center box-shadow group space-x-2 p-3 font-medium text-gray-300 duration-150 ease-in-out hover:text-white focus:outline-none">
+        <span>or Schedule a Demo</span>
       </a>
     </Link>
   );
@@ -19,10 +19,10 @@ function ATFButtonStart() {
   return (
     <a href="https://app.touca.io" target="_blank" rel="noopener noreferrer">
       <button
-        className="box-shadow text-lg group space-x-2 rounded-xl bg-dark-blue-700 bg-opacity-25 p-3 font-medium text-white shadow-[0_0_5px_#7dd3fc] duration-150 ease-in-out hover:bg-opacity-50 focus:outline-none"
+        className="box-shadow text-lg group space-x-2 rounded-full bg-dark-blue-700 bg-opacity-25 py-3 px-8 font-medium text-white shadow-[0_0_7px_#7dd3fc] duration-150 ease-in-out hover:bg-opacity-50 focus:outline-none"
         type="button"
         role="button">
-        <span>Start for Free</span>
+        <span>Start with $100 Credits</span>
         <HiArrowNarrowRight className="sm:inline hidden h-6 opacity-50 group-hover:opacity-100" />
       </button>
     </a>
@@ -32,7 +32,7 @@ function ATFButtonStart() {
 function ATFTagline() {
   return (
     <div className="space-y-4">
-      <h2 className="max-w-3xl lg:max-w-4xl sm:text-center text-white font-extrabold text-3xl md:text-4xl lg:text-7xl ">
+      <h2 className="max-w-3xl lg:max-w-4xl sm:text-center text-white font-extrabold text-3xl md:text-4xl lg:text-7xl">
         Find regressions in{' '}
         <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-yellow-600 to-yellow-300">
           minutes
@@ -66,7 +66,7 @@ export function AboveTheFold() {
     <section className="bg-gradient-to-b from-dark-blue-900 to-dark-blue-800">
       <div className="grid place-content-center min-h-[calc(100vh-5rem)] sm:min-h-[calc(100vh-5rem-15vh)] space-y-16 px-4 sm:px-8">
         <ATFTagline />
-        <div className="flex justify-center space-x-4">
+        <div className="sm:space-x-2 flex flex-col sm:flex-row items-center sm:justify-center">
           <ATFButtonStart />
           <ATFButtonDemo />
         </div>
