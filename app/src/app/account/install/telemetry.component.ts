24fc25755c0cfe4aeb64a9c57100838045b0bff9
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2022-08-08 10:05:14 -0700

    server: remove toggle from telemetry tab

--- app/src/app/account/install/telemetry.component.ts
+++ app/src/app/account/install/telemetry.component.ts
@@ -2,57 +2,49 @@
 
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, EventEmitter, Output } from '@angular/core';
-import { Router } from '@angular/router';
 
 import {
   ELocalStorageKey,
   InstallPageTabType
 } from '@/core/models/frontendtypes';
 import { ApiService } from '@/core/services';
-import { Checkbox } from '@/shared/components/checkbox.component';
 
 @Component({
   selector: 'app-account-install-telemetry',
   templateUrl: './telemetry.component.html'
 })
 export class InstallTelemetryComponent {
-  preference = {
-    default: true,
-    description: 'Can we collect aggregate anonymous usage data?',
-    experimental: false,
-    saved: false,
-    slug: 'telemetry',
-    title: 'Anonymous Usage Data',
-    visible: true
-  };
-  telemetry = this.preference.default;
+  sample_data = `{
+  "createdAt": "2022-08-08T15:29:24.325Z",
+  "messages_new": 5814,
+  "node_id": "a7cea1d8-851a-4e2a-b20b-a98a68dab943",
+  "reports_new": 7,
+  "runtime_new": 657,
+  "sessions_new": 14,
+  "users_active": 2,
+  "versions_new": 6
+}`;
 
   @Output() switchTab = new EventEmitter<{
     error?: string;
     next?: InstallPageTabType;
   }>();
 
-  constructor(private apiService: ApiService, private router: Router) {}
+  constructor(private apiService: ApiService) {}
 
-  toggleFeatureFlag(flag: Checkbox) {
-    this.telemetry = flag.value;
-  }
-
-  submitTelemetry() {
-    this.apiService
-      .patch('/platform/config', { telemetry: !!this.telemetry })
-      .subscribe({
-        next: (doc: { url?: string }) => {
-          this.apiService._status = undefined;
-          this.switchTab.emit({ next: 'thanks' });
-          if (doc?.url) {
-            localStorage.setItem(ELocalStorageKey.ActivationKey, doc.url);
-          }
-        },
-        error: (err: HttpErrorResponse) => {
-          const error = this.apiService.extractError(err, []);
-          this.switchTab.emit({ error });
+  proceed() {
+    this.apiService.patch('/platform/config', { telemetry: true }).subscribe({
+      next: (doc: { url?: string }) => {
+        this.apiService._status = undefined;
+        this.switchTab.emit({ next: 'thanks' });
+        if (doc?.url) {
+          localStorage.setItem(ELocalStorageKey.ActivationKey, doc.url);
         }
-      });
+      },
+      error: (err: HttpErrorResponse) => {
+        const error = this.apiService.extractError(err, []);
+        this.switchTab.emit({ error });
+      }
+    });
   }
 }
