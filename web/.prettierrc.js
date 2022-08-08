004d36b0ec02488fe8b552b299fcd36745b3a55d
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2022-08-08 10:06:31 -0700

    web: fix tailwind plugin for prettier

---
+++ web/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  htmlWhitespaceSensitivity: 'ignore',
+  jsxBracketSameLine: true,
+  proseWrap: 'always',
+  plugins: [require('prettier-plugin-tailwindcss')]
+};
