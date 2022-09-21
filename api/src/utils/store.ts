baa762c6f5206d827648631a41a529fc965457b8
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2022-09-21 00:25:18 -0700

    server: update object store migration

--- api/src/utils/store.ts
+++ api/src/utils/store.ts
@@ -154,19 +154,14 @@ class S3ObjectStore extends ObjectStore {
     return this.streamToBuffer(data.Body)
   }
   async upgradeBuckets() {
-    const suffixes: ('comparisons' | 'messages' | 'results')[] = [
-      'comparisons',
-      'messages',
-      'results'
-    ]
-    for (const bucketSuffix of suffixes) {
-      if (!(await this.bucketExists(`touca-${bucketSuffix}`))) {
+    for (const suffix of ['comparisons', 'messages', 'results']) {
+      if (!(await this.bucketExists(`touca-${suffix}`))) {
         continue
       }
       let comparisons: ListObjectsV2CommandOutput
       do {
         comparisons = await this.client.send(
-          new ListObjectsV2Command({ Bucket: `touca-${bucketSuffix}` })
+          new ListObjectsV2Command({ Bucket: `touca-${suffix}` })
         )
         if (!comparisons.Contents) {
           break
@@ -174,22 +169,20 @@ class S3ObjectStore extends ObjectStore {
         for (const obj of comparisons.Contents) {
           const data = await this.client.send(
             new GetObjectCommand({
-              Bucket: `touca-${bucketSuffix}`,
+              Bucket: `touca-${suffix}`,
               Key: obj.Key
             })
           )
           const transform =
-            bucketSuffix === 'messages'
-              ? this.streamToBuffer
-              : this.streamToString
+            suffix === 'messages' ? this.streamToBuffer : this.streamToString
           await this.putDocument(
-            bucketSuffix,
+            suffix as 'comparisons' | 'messages' | 'results',
             obj.Key,
             await transform(data.Body)
           )
           await this.client.send(
             new DeleteObjectCommand({
-              Bucket: `touca-${bucketSuffix}`,
+              Bucket: `touca-${suffix}`,
               Key: obj.Key
             })
           )
@@ -197,7 +190,7 @@ class S3ObjectStore extends ObjectStore {
       } while (comparisons.KeyCount !== 0)
       if (!config.isCloudHosted) {
         await this.client.send(
-          new DeleteBucketCommand({ Bucket: `touca-${bucketSuffix}` })
+          new DeleteBucketCommand({ Bucket: `touca-${suffix}` })
         )
       }
     }
