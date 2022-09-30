ae09cda53a6272fee4629d96e2fb396e492b3913
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2022-09-29 20:41:54 -0700

    sdk-python: bump development version to v1.8.0

--- sdk/python/touca/__init__.py
+++ sdk/python/touca/__init__.py
@@ -22,7 +22,7 @@ from touca._client import Client
 from touca._runner import Workflow, workflow, run
 from touca._utils import scoped_timer
 
-__version__ = "1.5.9"
+__version__ = "1.8.0"
 
 
 def clientmethod(f):
