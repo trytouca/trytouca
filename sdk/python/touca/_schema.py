bbf3f7e9f1ca3f0032b83d385b58e3040b647a71
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2022-10-02 20:58:47 -0700

    sdk-python: switch to using flatc

--- sdk/python/touca/_schema.py
+++ sdk/python/touca/_schema.py
@@ -1,6 +1,136 @@
 import flatbuffers
 
 
+class Type(object):
+    NONE = 0
+    Bool = 1
+    Int = 2
+    UInt = 3
+    Float = 4
+    Double = 5
+    String = 6
+    Object = 7
+    Array = 8
+    Blob = 9
+
+
+class ResultType(object):
+    Check = 1
+    Assert = 2
+
+
+def TypeWrapperStart(builder):
+    builder.StartObject(2)
+
+
+def TypeWrapperAddValueType(builder, valueType):
+    builder.PrependUint8Slot(0, valueType, 0)
+
+
+def TypeWrapperAddValue(builder, value):
+    builder.PrependUOffsetTRelativeSlot(
+        1, flatbuffers.number_types.UOffsetTFlags.py_type(value), 0
+    )
+
+
+def TypeWrapperEnd(builder):
+    return builder.EndObject()
+
+
+def BoolStart(builder):
+    builder.StartObject(1)
+
+
+def BoolAddValue(builder, value):
+    builder.PrependBoolSlot(0, value, 0)
+
+
+def BoolEnd(builder):
+    return builder.EndObject()
+
+
+def IntStart(builder):
+    builder.StartObject(1)
+
+
+def IntAddValue(builder, value):
+    builder.PrependInt64Slot(0, value, 0)
+
+
+def IntEnd(builder):
+    return builder.EndObject()
+
+
+def DoubleStart(builder):
+    builder.StartObject(1)
+
+
+def DoubleAddValue(builder, value):
+    builder.PrependFloat64Slot(0, value, 0.0)
+
+
+def DoubleEnd(builder):
+    return builder.EndObject()
+
+
+def StringStart(builder):
+    builder.StartObject(1)
+
+
+def StringAddValue(builder, value):
+    builder.PrependUOffsetTRelativeSlot(
+        0, flatbuffers.number_types.UOffsetTFlags.py_type(value), 0
+    )
+
+
+def StringEnd(builder):
+    return builder.EndObject()
+
+
+def ObjectMemberStart(builder):
+    builder.StartObject(2)
+
+
+def ObjectMemberAddName(builder, name):
+    builder.PrependUOffsetTRelativeSlot(
+        0, flatbuffers.number_types.UOffsetTFlags.py_type(name), 0
+    )
+
+
+def ObjectMemberAddValue(builder, value):
+    builder.PrependUOffsetTRelativeSlot(
+        1, flatbuffers.number_types.UOffsetTFlags.py_type(value), 0
+    )
+
+
+def ObjectMemberEnd(builder):
+    return builder.EndObject()
+
+
+def ObjectStart(builder):
+    builder.StartObject(2)
+
+
+def ObjectAddKey(builder, key):
+    builder.PrependUOffsetTRelativeSlot(
+        0, flatbuffers.number_types.UOffsetTFlags.py_type(key), 0
+    )
+
+
+def ObjectAddValues(builder, values):
+    builder.PrependUOffsetTRelativeSlot(
+        1, flatbuffers.number_types.UOffsetTFlags.py_type(values), 0
+    )
+
+
+def ObjectStartValuesVector(builder, numElems):
+    return builder.StartVector(4, numElems, 4)
+
+
+def ObjectEnd(builder):
+    return builder.EndObject()
+
+
 def ArrayStart(builder):
     builder.StartObject(1)
 
@@ -33,39 +163,137 @@ def BlobEnd(builder):
     return builder.EndObject()
 
 
-def BoolStart(builder):
-    builder.StartObject(1)
+def ResultStart(builder):
+    builder.StartObject(3)
 
 
-def BoolAddValue(builder, value):
-    builder.PrependBoolSlot(0, value, 0)
+def ResultAddKey(builder, key):
+    builder.PrependUOffsetTRelativeSlot(
+        0, flatbuffers.number_types.UOffsetTFlags.py_type(key), 0
+    )
 
 
-def BoolEnd(builder):
+def ResultAddValue(builder, value):
+    builder.PrependUOffsetTRelativeSlot(
+        1, flatbuffers.number_types.UOffsetTFlags.py_type(value), 0
+    )
+
+
+def ResultAddTyp(builder, typ):
+    builder.PrependUint8Slot(2, typ, 1)
+
+
+def ResultEnd(builder):
     return builder.EndObject()
 
 
-def DoubleStart(builder):
-    builder.StartObject(1)
+def AssertionStart(builder):
+    builder.StartObject(2)
 
 
-def DoubleAddValue(builder, value):
-    builder.PrependFloat64Slot(0, value, 0.0)
-
-
-def DoubleEnd(builder):
+def AssertionEnd(builder):
     return builder.EndObject()
 
 
-def IntStart(builder):
+def MetricStart(builder):
+    builder.StartObject(2)
+
+
+def MetricAddKey(builder, key):
+    builder.PrependUOffsetTRelativeSlot(
+        0, flatbuffers.number_types.UOffsetTFlags.py_type(key), 0
+    )
+
+
+def MetricAddValue(builder, value):
+    builder.PrependUOffsetTRelativeSlot(
+        1, flatbuffers.number_types.UOffsetTFlags.py_type(value), 0
+    )
+
+
+def MetricEnd(builder):
+    return builder.EndObject()
+
+
+def ResultsStart(builder):
     builder.StartObject(1)
 
 
-def IntAddValue(builder, value):
-    builder.PrependInt64Slot(0, value, 0)
+def ResultsAddEntries(builder, entries):
+    builder.PrependUOffsetTRelativeSlot(
+        0, flatbuffers.number_types.UOffsetTFlags.py_type(entries), 0
+    )
 
 
-def IntEnd(builder):
+def ResultsStartEntriesVector(builder, numElems):
+    return builder.StartVector(4, numElems, 4)
+
+
+def ResultsEnd(builder):
+    return builder.EndObject()
+
+
+def AssertionsStart(builder):
+    builder.StartObject(1)
+
+
+def AssertionsEnd(builder):
+    return builder.EndObject()
+
+
+def MetricsStart(builder):
+    builder.StartObject(1)
+
+
+def MetricsAddEntries(builder, entries):
+    builder.PrependUOffsetTRelativeSlot(
+        0, flatbuffers.number_types.UOffsetTFlags.py_type(entries), 0
+    )
+
+
+def MetricsStartEntriesVector(builder, numElems):
+    return builder.StartVector(4, numElems, 4)
+
+
+def MetricsEnd(builder):
+    return builder.EndObject()
+
+
+def MetadataStart(builder):
+    builder.StartObject(6)
+
+
+def MetadataAddTestsuite(builder, testsuite):
+    builder.PrependUOffsetTRelativeSlot(
+        0, flatbuffers.number_types.UOffsetTFlags.py_type(testsuite), 0
+    )
+
+
+def MetadataAddVersion(builder, version):
+    builder.PrependUOffsetTRelativeSlot(
+        1, flatbuffers.number_types.UOffsetTFlags.py_type(version), 0
+    )
+
+
+def MetadataAddTestcase(builder, testcase):
+    builder.PrependUOffsetTRelativeSlot(
+        3, flatbuffers.number_types.UOffsetTFlags.py_type(testcase), 0
+    )
+
+
+def MetadataAddBuiltAt(builder, builtAt):
+    builder.PrependUOffsetTRelativeSlot(
+        4, flatbuffers.number_types.UOffsetTFlags.py_type(builtAt), 0
+    )
+
+
+def MetadataAddTeamslug(builder, teamslug):
+    builder.PrependUOffsetTRelativeSlot(
+        5, flatbuffers.number_types.UOffsetTFlags.py_type(teamslug), 0
+    )
+
+
+def MetadataEnd(builder):
     return builder.EndObject()
 
 
@@ -109,6 +337,13 @@ def MessageBufferStartBufVector(builder, numElems):
     return builder.StartVector(1, numElems, 1)
 
 
+def MessageBufferMakeBufVectorFromBytes(builder, bytes):
+    builder.StartVector(1, len(bytes), 1)
+    builder.head = builder.head - len(bytes)
+    builder.Bytes[builder.head : builder.head + len(bytes)] = bytes
+    return builder.EndVector()
+
+
 def MessageBufferEnd(builder):
     return builder.EndObject()
 
@@ -128,216 +363,4 @@ def MessagesStartMessagesVector(builder, numElems):
 
 
 def MessagesEnd(builder):
-    return builder.EndObject()
-
-
-def MetadataStart(builder):
-    builder.StartObject(6)
-
-
-def MetadataAddTestsuite(builder, testsuite):
-    builder.PrependUOffsetTRelativeSlot(
-        0, flatbuffers.number_types.UOffsetTFlags.py_type(testsuite), 0
-    )
-
-
-def MetadataAddVersion(builder, version):
-    builder.PrependUOffsetTRelativeSlot(
-        1, flatbuffers.number_types.UOffsetTFlags.py_type(version), 0
-    )
-
-
-def MetadataAddTestcase(builder, testcase):
-    builder.PrependUOffsetTRelativeSlot(
-        3, flatbuffers.number_types.UOffsetTFlags.py_type(testcase), 0
-    )
-
-
-def MetadataAddBuiltAt(builder, builtAt):
-    builder.PrependUOffsetTRelativeSlot(
-        4, flatbuffers.number_types.UOffsetTFlags.py_type(builtAt), 0
-    )
-
-
-def MetadataAddTeamslug(builder, teamslug):
-    builder.PrependUOffsetTRelativeSlot(
-        5, flatbuffers.number_types.UOffsetTFlags.py_type(teamslug), 0
-    )
-
-
-def MetadataEnd(builder):
-    return builder.EndObject()
-
-
-def MetricStart(builder):
-    builder.StartObject(2)
-
-
-def MetricAddKey(builder, key):
-    builder.PrependUOffsetTRelativeSlot(
-        0, flatbuffers.number_types.UOffsetTFlags.py_type(key), 0
-    )
-
-
-def MetricAddValue(builder, value):
-    builder.PrependUOffsetTRelativeSlot(
-        1, flatbuffers.number_types.UOffsetTFlags.py_type(value), 0
-    )
-
-
-def MetricEnd(builder):
-    return builder.EndObject()
-
-
-def MetricsStart(builder):
-    builder.StartObject(1)
-
-
-def MetricsAddEntries(builder, entries):
-    builder.PrependUOffsetTRelativeSlot(
-        0, flatbuffers.number_types.UOffsetTFlags.py_type(entries), 0
-    )
-
-
-def MetricsStartEntriesVector(builder, numElems):
-    return builder.StartVector(4, numElems, 4)
-
-
-def MetricsEnd(builder):
-    return builder.EndObject()
-
-
-def ObjectStart(builder):
-    builder.StartObject(2)
-
-
-def ObjectAddKey(builder, key):
-    builder.PrependUOffsetTRelativeSlot(
-        0, flatbuffers.number_types.UOffsetTFlags.py_type(key), 0
-    )
-
-
-def ObjectAddValues(builder, values):
-    builder.PrependUOffsetTRelativeSlot(
-        1, flatbuffers.number_types.UOffsetTFlags.py_type(values), 0
-    )
-
-
-def ObjectStartValuesVector(builder, numElems):
-    return builder.StartVector(4, numElems, 4)
-
-
-def ObjectEnd(builder):
-    return builder.EndObject()
-
-
-def ObjectMemberStart(builder):
-    builder.StartObject(2)
-
-
-def ObjectMemberAddName(builder, name):
-    builder.PrependUOffsetTRelativeSlot(
-        0, flatbuffers.number_types.UOffsetTFlags.py_type(name), 0
-    )
-
-
-def ObjectMemberAddValue(builder, value):
-    builder.PrependUOffsetTRelativeSlot(
-        1, flatbuffers.number_types.UOffsetTFlags.py_type(value), 0
-    )
-
-
-def ObjectMemberEnd(builder):
-    return builder.EndObject()
-
-
-def ResultStart(builder):
-    builder.StartObject(3)
-
-
-def ResultAddKey(builder, key):
-    builder.PrependUOffsetTRelativeSlot(
-        0, flatbuffers.number_types.UOffsetTFlags.py_type(key), 0
-    )
-
-
-def ResultAddValue(builder, value):
-    builder.PrependUOffsetTRelativeSlot(
-        1, flatbuffers.number_types.UOffsetTFlags.py_type(value), 0
-    )
-
-
-def ResultAddTyp(builder, typ):
-    builder.PrependUint8Slot(2, typ, 1)
-
-
-def ResultEnd(builder):
-    return builder.EndObject()
-
-
-class ResultType(object):
-    Check = 1
-    Assert = 2
-
-
-def ResultsStart(builder):
-    builder.StartObject(1)
-
-
-def ResultsAddEntries(builder, entries):
-    builder.PrependUOffsetTRelativeSlot(
-        0, flatbuffers.number_types.UOffsetTFlags.py_type(entries), 0
-    )
-
-
-def ResultsStartEntriesVector(builder, numElems):
-    return builder.StartVector(4, numElems, 4)
-
-
-def ResultsEnd(builder):
-    return builder.EndObject()
-
-
-def StringStart(builder):
-    builder.StartObject(1)
-
-
-def StringAddValue(builder, value):
-    builder.PrependUOffsetTRelativeSlot(
-        0, flatbuffers.number_types.UOffsetTFlags.py_type(value), 0
-    )
-
-
-def StringEnd(builder):
-    return builder.EndObject()
-
-
-class Type(object):
-    NONE = 0
-    Bool = 1
-    Int = 2
-    UInt = 3
-    Float = 4
-    Double = 5
-    String = 6
-    Object = 7
-    Array = 8
-    Blob = 9
-
-
-def TypeWrapperStart(builder):
-    builder.StartObject(2)
-
-
-def TypeWrapperAddValueType(builder, valueType):
-    builder.PrependUint8Slot(0, valueType, 0)
-
-
-def TypeWrapperAddValue(builder, value):
-    builder.PrependUOffsetTRelativeSlot(
-        1, flatbuffers.number_types.UOffsetTFlags.py_type(value), 0
-    )
-
-
-def TypeWrapperEnd(builder):
     return builder.EndObject()
