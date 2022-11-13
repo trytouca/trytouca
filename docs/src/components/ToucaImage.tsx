4698caacd94471f6eb90eff722cff00ad0dc32bd
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2022-11-12 21:21:29 -0800

    docs: upgrade dependencies

--- docs/src/components/ToucaImage.tsx
+++ docs/src/components/ToucaImage.tsx
@@ -5,7 +5,8 @@ import { useColorMode } from "@docusaurus/theme-common";
 type Props = { src: string; dark?: string; alt?: string; title?: string };
 
 export default function ToucaImage(props: Props) {
-  const { colorMode } = useColorMode();
+  // const { colorMode } = useColorMode();
+  const { colorMode } = { colorMode: "dark" };
   const imagePath = colorMode === "dark" && props.dark ? props.dark : props.src;
   return (
     <Image
