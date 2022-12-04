7d778720cedae0e5f381d7eb98559a5643e5146b
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2022-12-03 20:48:44 -0800

    sdk-js: add support for programmatic testcase declaration

--- sdk/js/packages/node/src/index.ts
+++ sdk/js/packages/node/src/index.ts
@@ -1,6 +1,7 @@
 // Copyright 2022 Touca, Inc. Subject to Apache-2.0 License.
 
 import { NodeClient } from './client';
+export { VERSION } from './version';
 
 export const touca = new NodeClient();
 export default touca;
---
+++ sdk/js/packages/node/src/version.ts
@@ -0,0 +1,3 @@
+// Copyright 2022 Touca, Inc. Subject to Apache-2.0 License.
+
+export const VERSION = '1.5.7';
