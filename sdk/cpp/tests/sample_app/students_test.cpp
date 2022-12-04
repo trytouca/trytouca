426d959cb09c0e7909b5da5b348ae87edc1b5c82
Pejman Ghorbanzade <pejman@touca.io>
2022-12-03 17:28:29 -0800

    sdk-cpp: update sample app (#421)

---
+++ sdk/cpp/tests/sample_app/students_test.cpp
@@ -0,0 +1,18 @@
+// Copyright 2022 Touca, Inc. Subject to Apache-2.0 License.
+
+#include "students.hpp"
+
+#include "students_types.hpp"
+#include "touca/touca.hpp"
+
+int main(int argc, char* argv[]) {
+  touca::workflow("find_student", [](const std::string& username) {
+    const auto& student = find_student(username);
+    touca::assume("username", student.username);
+    touca::check("fullname", student.fullname);
+    touca::check("birth_date", student.dob);
+    touca::check("gpa", student.gpa);
+    touca::add_metric("external_source", 1500);
+  });
+  return touca::run(argc, argv);
+}
