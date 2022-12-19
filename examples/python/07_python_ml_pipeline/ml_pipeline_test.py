6b71a3a5847c82cfe1d6d29005750d0eddd5e4dd
Ryan Marr <r.marr747@gmail.com>
2022-12-19 00:14:52 -0800

    examples: add addional python example for testing ml pipelines (#208)
    Co-authored-by: Ryan Marr <ryanmarr@Ryans-Air.attlocal.net>

---
+++ examples/python/07_python_ml_pipeline/ml_pipeline_test.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Touca, Inc. Subject to Apache-2.0 License.
+
+import touca
+import joblib
+from pathlib import Path
+import csv
+
+out_dir = Path("out")
+pipeline = joblib.load(out_dir.joinpath("pipeline.bin"))
+
+reader = csv.reader(out_dir.joinpath("testcases.csv").open(mode="r"))
+inputs = {row[0]: row[1:] for row in reader}
+
+
+@touca.workflow(testcases=list(inputs.keys()))
+def pipeline_test(name: str):
+    data = [float(x) for x in inputs[name]]
+    y_test = pipeline.predict([data])[0]
+    touca.check("stage", y_test.item())
