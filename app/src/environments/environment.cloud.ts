3bdce91be1196fe274b124722100a3220c6ec920
Pejman Ghorbanzade <pejman@touca.io>
2022-12-19 23:13:41 -0800

    server: publish event after each message comparison (#461)

--- app/src/environments/environment.cloud.ts
+++ app/src/environments/environment.cloud.ts
@@ -4,7 +4,7 @@ export const environment = {
   apiUrl: 'https://api.touca.io',
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   appVersion: require('../../package.json').version,
-  dataRefreshInterval: 10000,
+  dataRefreshInterval: 60_000,
   google_api_client_id:
     '232474004917-li0ioanjkoi6lj6vq2intei2kv7op5li.apps.googleusercontent.com',
   self_hosted: false,
--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   apiUrl: '/api',
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   appVersion: require('../../package.json').version,
-  dataRefreshInterval: 10000,
+  dataRefreshInterval: 60_000,
   google_api_client_id: '',
   self_hosted: true,
   production: true
