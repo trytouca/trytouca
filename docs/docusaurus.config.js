38a10a3a4e686239ad8a04a88e9ded744fd60b78
Pejman Ghorbanzade <pejman@touca.io>
2023-01-27 21:18:22 -0800

    docs: update color of sidebar in mobile view

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
         },
         blog: false,
         theme: {
-          customCss: require.resolve("./src/css/custom.css")
+          customCss: require.resolve("./src/css/overrides.css")
         }
       })
     ]
---
+++ docs/src/components/since.tsx
@@ -0,0 +1,15 @@
+// Copyright 2023 Touca, Inc. Subject to Apache-2.0 License.
+
+import React from "react";
+import styles from "./since.module.css";
+
+export default function Since({ version }): JSX.Element {
+  return (
+    <div className={styles.since}>
+      <div>
+        <span>Since: </span>
+        <span>{version}</span>
+      </div>
+    </div>
+  );
+}
