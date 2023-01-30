8e2ca86411acd7fbfb54a24ecf61de8f1abd980d
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2023-01-30 09:37:32 -0800

    sdk-python: improve demo plugin

--- sdk/python/touca/cli/plugins/demo.py
+++ sdk/python/touca/cli/plugins/demo.py
@@ -1,11 +1,16 @@
 # Copyright 2023 Touca, Inc. Subject to Apache-2.0 License.
 
 from touca.cli.common import CliCommand
+from argparse import ArgumentParser
 
 
 class DownloadCommand(CliCommand):
     name = "download"
     help = "Downloads sample output of getting started tutorial"
+
+    @classmethod
+    def parser(cls, parser: ArgumentParser):
+        parser.add_argument("version", help="version", choices=["v1.0", "v1.1"])
 
     def run(self):
         from urllib.request import urlopen
@@ -13,11 +18,14 @@ class DownloadCommand(CliCommand):
         from tempfile import TemporaryDirectory
         from shutil import unpack_archive
 
-        extract_dir = Path.cwd().joinpath("tutorial")
+        version = self.options.get("version")
+        extract_dir = Path.cwd().joinpath("tutorial", version)
         if extract_dir.exists():
             raise RuntimeError("tutorial directory already exists")
 
-        with urlopen("https://touca.io/docs/external/assets/tutorial.zip") as response:
+        with urlopen(
+            f"https://touca.io/docs/external/assets/tutorial/{version}.zip"
+        ) as response:
             content = response.read()
 
         with TemporaryDirectory() as dirname:
