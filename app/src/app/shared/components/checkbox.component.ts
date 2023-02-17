e41ceea17d9b4ad654dee95e43e21a4f7388b78c
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2023-02-17 12:57:42 -0800

    simplify checkbox

--- app/src/app/shared/components/checkbox.component.ts
+++ app/src/app/shared/components/checkbox.component.ts
@@ -1,4 +1,4 @@
-// Copyright 2021 Touca, Inc. Subject to Apache-2.0 License.
+// Copyright 2023 Touca, Inc. Subject to Apache-2.0 License.
 
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
@@ -15,17 +15,22 @@ export interface Checkbox {
 
 @Component({
   selector: 'app-settings-checkbox',
-  templateUrl: './checkbox.component.html',
-  styles: [
-    `
-      input:checked ~ .wsl-checkbox-line {
-        background-color: #0284c7;
-      }
-      input:checked ~ .wsl-checkbox-dot {
-        transform: translateX(100%);
-      }
-    `
-  ]
+  template: `
+    <div class="flex items-center justify-between">
+      <div class="space-x-1">
+        <span class="mb-1 text-sm font-medium text-gray-700 dark:text-gray-200">
+          {{ data.title }}
+        </span>
+      </div>
+      <div class="flex items-center space-x-1">
+        <span class="text-xs text-green-600" *ngIf="data.saved">Saved</span>
+        <app-checkbox [value]="data.value" (toggle)="toggle.emit()" />
+      </div>
+    </div>
+    <small class="wsl-text-muted" id="wsl-beta-color-help">
+      {{ data.description }}
+    </small>
+  `
 })
 export class CheckboxComponent {
   @Input() data: Checkbox;
